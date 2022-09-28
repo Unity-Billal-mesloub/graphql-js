5745ef20e97d782a225a93442b495fb4044eb5f9
Christoph Zwerschke <cito@online.de>
2022-09-28 13:12:45 +0200

    Fix typo in the error message for max tokens (#3748)

--- src/language/__tests__/parser-test.ts
+++ src/language/__tests__/parser-test.ts
@@ -97,22 +97,22 @@ describe('Parser', () => {
     `);
   });
 
-  it('exposes the tokenCount', () => {
-    expect(parse('{ foo }').tokenCount).to.equal(3);
-    expect(parse('{ foo(bar: "baz") }').tokenCount).to.equal(8);
-  });
-
-  it('limit maximum number of tokens', () => {
+  it('limits by a maximum number of tokens', () => {
     expect(() => parse('{ foo }', { maxTokens: 3 })).to.not.throw();
     expect(() => parse('{ foo }', { maxTokens: 2 })).to.throw(
-      'Syntax Error: Document contains more that 2 tokens. Parsing aborted.',
+      'Syntax Error: Document contains more than 2 tokens. Parsing aborted.',
     );
 
     expect(() => parse('{ foo(bar: "baz") }', { maxTokens: 8 })).to.not.throw();
 
     expect(() => parse('{ foo(bar: "baz") }', { maxTokens: 7 })).to.throw(
-      'Syntax Error: Document contains more that 7 tokens. Parsing aborted.',
+      'Syntax Error: Document contains more than 7 tokens. Parsing aborted.',
     );
+  });
+
+  it('exposes the tokenCount', () => {
+    expect(parse('{ foo }').tokenCount).to.equal(3);
+    expect(parse('{ foo(bar: "baz") }').tokenCount).to.equal(8);
   });
 
   it('parses variable inline values', () => {
