e5ed7506d2d280fc97f0fe1f77671887e0acae23
GitHub Action Script <please@open.issue>
2026-06-09 08:09:54 +0000

    Deploy 6e2771174f03f807b9aa5d64a1cd51def9a73b3b to 'npm' branch

--- utilities/extendSchema.js
+++ utilities/extendSchema.js
@@ -367,10 +367,14 @@ function extendSchemaImpl(schemaConfig, documentAST, options) {
                 }
                 case kinds_ts_1.Kind.SCALAR_TYPE_DEFINITION: {
                     const extensionASTNodes = scalarExtensions.get(name) ?? [];
+                    let specifiedByURL = getSpecifiedByURL(astNode);
+                    for (const extensionNode of extensionASTNodes) {
+                        specifiedByURL = getSpecifiedByURL(extensionNode) ?? specifiedByURL;
+                    }
                     return new definition_ts_1.GraphQLScalarType({
                         name,
                         description: astNode.description?.value,
-                        specifiedByURL: getSpecifiedByURL(astNode),
+                        specifiedByURL,
                         astNode,
                         extensionASTNodes,
                     });
--- utilities/extendSchema.mjs
+++ utilities/extendSchema.mjs
@@ -363,10 +363,14 @@ export function extendSchemaImpl(schemaConfig, documentAST, options) {
                 }
                 case Kind.SCALAR_TYPE_DEFINITION: {
                     const extensionASTNodes = scalarExtensions.get(name) ?? [];
+                    let specifiedByURL = getSpecifiedByURL(astNode);
+                    for (const extensionNode of extensionASTNodes) {
+                        specifiedByURL = getSpecifiedByURL(extensionNode) ?? specifiedByURL;
+                    }
                     return new GraphQLScalarType({
                         name,
                         description: astNode.description?.value,
-                        specifiedByURL: getSpecifiedByURL(astNode),
+                        specifiedByURL,
                         astNode,
                         extensionASTNodes,
                     });
