bd775ff61af7f68fda8054100a0d3b089cd7d8b3
GitHub Action Script <please@open.issue>
2026-06-15 17:13:12 +0000

    Deploy c7e494ad58d9aaef49a9174d886b1f322311d77f to 'npm' branch

--- version.js
+++ version.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.versionInfo = exports.version = void 0;
-exports.version = '17.0.0-rc.0';
+exports.version = '17.0.0';
 exports.versionInfo = Object.freeze({
     major: 17,
     minor: 0,
     patch: 0,
-    preReleaseTag: 'rc.0',
+    preReleaseTag: null,
 });
 //# sourceMappingURL=version.js.map
--- version.mjs
+++ version.mjs
@@ -1,8 +1,8 @@
-export const version = '17.0.0-rc.0';
+export const version = '17.0.0';
 export const versionInfo = Object.freeze({
     major: 17,
     minor: 0,
     patch: 0,
-    preReleaseTag: 'rc.0',
+    preReleaseTag: null,
 });
 //# sourceMappingURL=version.js.map
